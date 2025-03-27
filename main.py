acc5004489d8a05499ac940fc64f167f6bdb9e83
ngkhanggg <nguyenkhangchu2004@gmail.com>
2025-03-27 14:39:23 +0700

    feat: create a class for data reconciliation
    check for missing and invalid records

--- main.py
+++ main.py
@@ -173,6 +173,31 @@ class DQTool(ABC):
         self.source_data = get_data_source('source')
         self.dest_data = get_data_source('dest')
 
+# ========================= Class DataReconciliation =========================
+
+class DataReconciliation(DQTool):
+    # Missing records
+    # The primary_key is in source but not in dest
+    def get_missing_records(self):
+        return self.hashed_source_data.join(
+            other=self.hashed_dest_data,
+            on='primary_key',
+            how='left_anti'
+        )
+
+    # Invalid records
+    # The same primary_key is in both source and dest, but their hash_key columns are not the same
+    def get_invalid_records(self):
+        merged_data = self.hashed_source_data.alias('src').join(
+            other=self.hashed_dest_data.alias('dest'),
+            on='primary_key',
+            how='inner'
+        )
+
+        invalid_records = merged_data.filter(col('src.hash_key') != col('dest.hash_key'))
+
+        return invalid_records
+
 # ========================= Main =========================
 
 job.commit()
